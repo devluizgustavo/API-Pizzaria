d9967fec14370057ee1704da88aba192f5caa1c4
Luiz Gustavo <luizgustavobonke@gmail.com>
2025-02-14 17:21:07 -0300

    :sparkles: feat: Rotas de endereços de clientes

--- src/routes/CustomerRoutes.js
+++ src/routes/CustomerRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import CustomerController from "../controllers/CustomerController.js";
+import AddressController from "../controllers/AddressController.js";
 import loginRequired from "../middleware/loginRequired.js";
 const router = new Router();
 
@@ -9,6 +10,14 @@ router.get("/:id", loginRequired, CustomerController.show);
 router.post("/", loginRequired, CustomerController.store);
 router.put("/:id", loginRequired, CustomerController.update);
 router.delete("/:id", loginRequired, CustomerController.delete);
+
+// Rotas de endereço do cliente
+router.get("/:idCustomer/address", loginRequired, AddressController.index);
+router.get("/:idCustomer/address/:zipCode",loginRequired, AddressController.show);
+router.post("/:idCustomer/address/",loginRequired, AddressController.store);
+router.put("/:idCustomer/address/:idAddress", loginRequired, AddressController.update);
+router.delete("/:idCustomer/address/:idAddress", loginRequired, AddressController.delete);
+
 
 export default router;
 
