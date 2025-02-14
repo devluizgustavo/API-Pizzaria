c1358af7c8d5261ab045f84bd8db07ab973f438b
Luiz Gustavo <luizgustavobonke@gmail.com>
2025-02-14 11:13:02 -0300

    :recycle: refactor: Refatoração do Controller de Clientes

---
+++ src/controllers/CustomerController.js
@@ -0,0 +1,179 @@
+import Customer from '../models/Customer.js';
+import Address from '../models/Address.js';
+import Contact from '../models/Contact.js';
+import { Sequelize } from 'sequelize';
+
+import database from '../config/database.js';
+
+class CustomerController {
+  async index(req, res) {
+    try {
+      const customers = await Customer.findAll({
+        attributes: ["id","cpf","name","lastname"],
+        include: [
+          {
+            model: Contact,
+            attributes: ["number_phone", "email"]
+          },
+          {
+            model: Address,
+            attributes: ["zip_code","street","neighborhood","city","state","house_number","complement"]
+          }
+        ]
+      });
+
+      if (!customers) {
+        return res.status(400).json({ errors: ["Nenhum cliente foi encontrado"] });
+      }
+
+      return res.status(200).json({ customers });
+    } catch (e) {
+      console.error(e);
+      if (e.errors) {
+        return res.status(404).json({
+          errors: e.errors.map(err => err.message)
+        })
+      }
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const customer = await Customer.findByPk(id,{
+        attributes: ["id", "name", "lastname", "cpf"],
+        include: [
+          {
+            model: Address,
+            attributes: ["zip_code","street","neighborhood","city","state","house_number","complement"]
+          }
+        ]
+      });
+
+      if (!customer) {
+        return res.status(400).json({ errors: ["O cliente não foi encontrado."] });
+      }
+
+      return res.status(200).json(customer);
+    } catch(e) {
+      return res.status(404).json({
+        errors: e.errors.map(err => err.message)
+      })
+    }
+  }
+
+  async store(req, res) {
+    // Pegar os dados do cliente na requisição
+    const { cpf, name, lastname, contactList, addressList } = req.body;
+    const userFromTokenID = req.id_user;
+
+    // Instanciar o Sequelize
+    const sequelize = new Sequelize(database);
+    const transaction = await sequelize.transaction();
+
+    try {
+      // Caso dados pessoais do cliente não sejam enviados
+      if (!cpf || !name || !lastname) {
+        return res.status(400).json({ errors: ["Nome, CPF e Sobrenome são dados obrigatórios."] });
+      }
+      if (!contactList || contactList.length === 0) {
+        return res.status(400).json({ errors: ["É necessário que haja ao menos 1 telefone para contato.", "É necessário que ao menos um endereço seja cadastrado."] });
+      }
+      if (!addressList || addressList.length === 0) {
+        return res.status(400).json({ errors: ["O cliente precisa ter ao menos um endereço cadastrado."] });
+      }
+
+      // Caso não houver nenhum erro, crie o cliente
+      const customer = await Customer.create({ cpf, name, lastname, id_user: userFromTokenID }, { transaction }); // Cliente
+
+      // Conferir se o cliente já existe
+      const clientExists = await Customer.findOne({ where: { cpf: cpf } });
+
+      if (clientExists) {
+        return res.status(400).json({ errors: ["O CPF já está vinculado a outro cadastro."] })
+      }
+
+      const contact = await Contact.create({ ...contactList, id_customer: customer.id }, { transaction }); // Contatos
+
+      const address = await Address.create({ ...addressList, id_customer: customer.id }, { transaction }); // Endereço
+
+      if (!contact || !address) {
+        // Caso haja algum erro com os dados enviados em contato ou endereço, faça o rollback na transação
+        await transaction.rollback();
+        // Mostrar erro
+        return res.status(400).json({ errors: ["Numero de contato e endereço PRECISAM ser preenchidos corretamente."] })
+      }
+
+      // Fazer commit da transação se caso der tudo certo
+      await transaction.commit();
+
+      // Retornar sucesso
+      return res.status(200).json({ msg: ["Cliente cadastrado com sucesso"] });
+    } catch (e) {
+      console.error(e);
+      // Se algo der errado, fazemos um rollback na transação
+      await transaction.rollback();
+      return res.status(404).json({
+        errors: e.errors.map(err => err.message)
+      })
+    }
+  }
+
+  async update(req, res) {
+    try {
+      // Pegar o ID enviado na requisição
+      const { id } = req.params;
+
+      // Procurar o cliente
+      const customer = await Customer.findByPk(id, {
+        attributes: ["id", "cpf", "name", "lastname"]
+      });
+
+      if (!customer) {
+        return res.status(400).json({
+          errors: ["O cliente não existe."]
+        });
+      }
+
+      // Atualizar os dados e mandar pra variável
+      const customerUpdate = await customer.update(req.body);
+
+      return res.status(200).json({ msg: "Dados Atualizados com Sucesso.", customerUpdate})
+    } catch (e) {
+      console.error(e);
+      if (e.errors) {
+        return res.status(404).json({
+          errors: e.errors.map(err => err.message)
+        });
+      }
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const customer = await Customer.findByPk(id);
+
+      if (!customer) {
+        return res.status(400).json({
+          errors: ["Cliente não existe"]
+        });
+      }
+
+      await customer.destroy();
+
+      return res.status(200).json({
+        msg: "Cliente deletado com sucesso."
+      });
+    } catch (e) {
+      console.error(e);
+      return res.status(404).json({
+        errors: e.errors.map(err => err.message)
+      });
+    }
+  }
+}
+
+export default new CustomerController();
