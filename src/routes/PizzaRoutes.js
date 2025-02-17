b57a96a9b69925d13f3bd54eefc7fd5f7725b548
Luiz Gustavo <luizgustavobonke@gmail.com>
2025-02-17 16:54:34 -0300

    :lock: security: Adicionando Middlewares nas rotas de pizza

--- src/routes/PizzaRoutes.js
+++ src/routes/PizzaRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import PizzaController from "../controllers/PizzaController";
+import loginRequired from '../middleware/loginRequired';
 const router = new Router();
 
 router.get('/', PizzaController.index);
 router.get('/:id', PizzaController.show);
-router.put("/:id", PizzaController.update);
-router.post("/", PizzaController.store);
-router.delete("/:id", PizzaController.delete);
+router.put("/:id", loginRequired, PizzaController.update);
+router.post("/", loginRequired, PizzaController.store);
+router.delete("/:id", loginRequired, PizzaController.delete);
 
 export default router;
